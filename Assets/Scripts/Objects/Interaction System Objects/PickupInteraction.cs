ae2ac14095945fa11cd6eb7c14069ca3aa9e2f76
Pratik5295 <pratik.shringarpure05@gmail.com>
2024-05-23 16:55:18 +0530

    Toaster message on object pick up

--- Assets/Scripts/Objects/Interaction System Objects/PickupInteraction.cs
+++ Assets/Scripts/Objects/Interaction System Objects/PickupInteraction.cs
@@ -1,3 +1,4 @@
+using KrazyKrakenGames.DetectiveGame.UI;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -5,6 +6,7 @@ namespace KrazyKrakenGames.DetectiveGame.Gameplay
 {
     public class PickupInteraction : InteractableObject
     {
+        [SerializeField] private StoryObjectSO objectData;
         public UnityEvent OnPickupComplete;
         
         public override void Interact()
@@ -17,6 +19,16 @@ namespace KrazyKrakenGames.DetectiveGame.Gameplay
             //Before destroying, add logic to update inventory
             triggerBox.gameObject.SetActive(false);
             gameObject.SetActive(false);
+
+            if (objectData != null)
+            {
+                var message = $"{objectData.ObjectName} has been picked up";
+                UIManager.instance.AddToasterMessage(message);
+            }
+            else
+            {
+                Debug.LogWarning("Pick up object missing data");
+            }
 
             OnPickupComplete?.Invoke();
 
